272122ea25550065eb446481126872b128925d68
Sergio Arroyo <largus@gmail.com>
2016-04-10 23:16:26 +0200

    Reformat project.clk

--- project.clj
+++ project.clj
@@ -2,17 +2,16 @@
   :description "Just me messing around with Clojure"
   :url "http://github.com/delr3ves/clojure-adviento"
   :license {
-             :name "Eclipse Public License"
-             :url "http://www.eclipse.org/legal/epl-v10.html"
-             }
+            :name "Eclipse Public License"
+            :url  "http://www.eclipse.org/legal/epl-v10.html"
+            }
   :dependencies [
-                  [org.clojure/clojure "1.8.0"]
-                  ]
-;  :main ^:skip-aot clojure-advento.core
+                 [org.clojure/clojure "1.8.0"]
+                 ]
   :target-path "target/%s"
-  :profiles {:dev {
-                    :plugins [[lein-midje "3.2"]]
-                    :dependencies [[midje "1.6.3"]]}
+  :profiles {:dev     {
+                       :plugins      [[lein-midje "3.2"]]
+                       :dependencies [[midje "1.6.3"]]}
              :uberjar {:aot :all}
              }
   )
